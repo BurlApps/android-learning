c7e96e41690323cc91997bab3c6b41e2719c0d0f
Nelson Ramirez <nelsonr462@gmail.com>
2015-08-25 11:56:13 -0700

    clickable list views implemented

--- Stormy/app/src/main/java/com/test/nelson/stormy/ui/DailyForecastActivity.java
+++ Stormy/app/src/main/java/com/test/nelson/stormy/ui/DailyForecastActivity.java
@@ -1,29 +1,65 @@
 package com.test.nelson.stormy.ui;
 
+import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.test.nelson.stormy.R;
+import com.test.nelson.stormy.adapters.DayAdapter;
+import com.test.nelson.stormy.weather.Day;
 
-public class DailyForecastActivity extends ListActivity {
+import java.util.Arrays;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class DailyForecastActivity extends Activity {
+    private Day[] mDays;
+
+    @Bind(android.R.id.list) ListView mListView;
+    @Bind(android.R.id.empty) TextView mEmptyTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
-        String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        ButterKnife.bind(this);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                daysOfTheWeek);
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
 
-        setListAdapter(adapter);
+        DayAdapter adapter = new DayAdapter(this, mDays);
+        mListView.setAdapter(adapter);
+        mListView.setEmptyView(mEmptyTextView);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+                String conditions = mDays[position].getSummary();
+                String highTemp = mDays[position].getTemperatureMax()+"";
+                String message = String.format("On %s the high will be %s and it will be %s",
+                        dayOfTheWeek,
+                        highTemp,
+                        conditions);
+
+                Toast.makeText(DailyForecastActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
-
 }
--- Stormy/app/src/main/java/com/test/nelson/stormy/weather/Day.java
+++ Stormy/app/src/main/java/com/test/nelson/stormy/weather/Day.java
@@ -1,9 +1,17 @@
 package com.test.nelson.stormy.weather;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Formatter;
+import java.util.TimeZone;
+
 /**
  * Created by nelson on 8/17/15.
  */
-public class Day {
+public class Day implements Parcelable {
     private long mTime;
     private String mSummary;
     private double mTemperatureMax;
@@ -28,8 +36,8 @@ public class Day {
         mSummary = summary;
     }
 
-    public double getTemperatureMax() {
-        return mTemperatureMax;
+    public int getTemperatureMax() {
+        return (int) Math.round(mTemperatureMax);
     }
 
     public void setTemperatureMax(double temperatureMax) {
@@ -51,4 +59,54 @@ public class Day {
     public void setTimeZone(String timeZone) {
         mTimeZone = timeZone;
     }
+
+    public int getIconId() {
+        return Forecast.getIconId(mIcon);
+    }
+
+    public String getDayOfTheWeek() {
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE");
+        formatter.setTimeZone(TimeZone.getTimeZone(mTimeZone));
+
+        Date dateTime = new Date(mTime * 1000);
+
+        return formatter.format(dateTime);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mTime);
+        dest.writeString(mSummary);
+        dest.writeDouble(mTemperatureMax);
+        dest.writeString(mIcon);
+        dest.writeString(mTimeZone);
+    }
+
+    private Day(Parcel in) {
+        mTime = in.readLong();
+        mSummary = in.readString();
+        mTemperatureMax = in.readDouble();
+        mIcon = in.readString();
+        mTimeZone = in.readString();
+    }
+
+    public Day() {}
+
+    public static final Creator<Day> CREATOR = new Creator<Day>() {
+        @Override
+        public Day createFromParcel(Parcel source) {
+            return new Day(source);
+        }
+
+        @Override
+        public Day[] newArray(int size) {
+            return new Day[size];
+        }
+    };
+
 }
